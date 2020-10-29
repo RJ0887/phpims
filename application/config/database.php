760dde7fd9ac64dd4b03280351d4a486bb7db0f7
RJ0887 <57557580+RJ0887@users.noreply.github.com>
2020-10-29 18:59:52 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '172.25.232.13',
+	'username' => 'admin',
+	'password' => 'admin',
 	'database' => 'stock',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
